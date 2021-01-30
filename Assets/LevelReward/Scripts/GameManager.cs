b8f774d42e13532a7674b76ffbc66a9c46ab329a
yu-aksak <65971020+yu-aksak@users.noreply.github.com>
2021-01-30 05:46:52 +0300

    loading

---
+++ Assets/LevelReward/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static int Level = 0;
+    [SerializeField] private Button levelUp;
+    [SerializeField] private Button getReward;
+    [SerializeField] private Button get;
+    [SerializeField] private Button leftReward;
+    [SerializeField] private Button rightReward;
+
+    [SerializeField] private GameObject canvasGetReward;
+    [SerializeField] private GameObject canvasReward;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI leftRewardText;
+    [SerializeField] private TextMeshProUGUI rightRewardText;
+    
+    private void Start()
+    {
+        levelUp.GetComponent<Button>().onClick.AddListener(LevelUp);
+        getReward.GetComponent<Button>().onClick.AddListener(GetReward);
+        get.GetComponent<Button>().onClick.AddListener(Get);
+        leftReward.GetComponent<Button>().onClick.AddListener(LeftReward);
+        rightReward.GetComponent<Button>().onClick.AddListener(RightReward);
+    }
+    
+    private void LevelUp()
+    {
+        canvasGetReward.SetActive(true);
+        levelUp.interactable = false;
+        Level++;
+        levelText.text = "Level: " + Level;
+    }
+
+    private void GetReward()
+    {
+        canvasGetReward.SetActive(false);
+        canvasReward.SetActive(true);
+        get.interactable = false;
+        SetColor(leftReward, Color.white);
+        SetColor(rightReward, Color.white);
+    }
+
+    private void Get()
+    {
+        string num;
+        canvasReward.SetActive(false);
+        levelUp.interactable = true;
+        if (!leftReward.interactable)
+            num = leftRewardText.text;
+        else
+            num = rightRewardText.text;
+        GameObject _text = GameObject.Find("x" + num);
+        string[] counts = _text.GetComponent<TextMeshProUGUI>().text.Split(' ');
+        int count = int.Parse(counts[1]);
+        count++;
+        _text.GetComponent<TextMeshProUGUI>().text = "x " + count;
+        
+        leftReward.interactable = true;
+        rightReward.interactable = true;
+    }
+    
+    private void LeftReward()
+    {
+        SetColor(leftReward, Color.green);
+        SetColor(rightReward, Color.white);
+        leftReward.interactable = false;
+        rightReward.interactable = true;
+        get.interactable = true;
+    }
+    
+    private void RightReward()
+    {
+        SetColor(leftReward, Color.white);
+        SetColor(rightReward, Color.green);
+        leftReward.interactable = true;
+        rightReward.interactable = false;
+        get.interactable = true;
+    }
+
+    private void SetColor(Button button, Color color)
+    {
+        button.GetComponent<Graphic>().color = color;
+    }
+}
---
+++ Assets/LevelReward/Scripts/GenerateButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI leftReward;
+    [SerializeField] private TextMeshProUGUI rightReward;
+    
+    // Start is called before the first frame update
+    void OnDisable()
+    {
+        if (GameManager.Level % 2 == 0)
+        {
+            leftReward.text = "1";
+        }
+        else
+        {
+            int num1 = Random_();
+            leftReward.text = num1.ToString();
+        }
+        int num2 = Random_();
+        rightReward.text = num2.ToString();
+    }
+
+    private int Random_()
+    {
+        int random = Random.Range(1, 100);
+        switch (random)
+        {
+            case int z when (z > 0 && z <= 15):
+                return 2;
+            case int z when (z >= 16 && z <= 30):
+                return 3;
+            case int z when (z >= 31 && z <= 45):
+                return 4;
+            case int z when (z >= 46 && z <= 50):
+                return 5;
+            case int z when (z >= 51 && z <= 65):
+                return 6;
+            case int z when (z >= 66 && z <= 80):
+                return 7;
+            case int z when (z >= 81 && z <= 95):
+                return 8;
+            case int z when (z >= 96 && z <= 100):
+                return 9;
+        }
+        return 0;
+    }
+}
